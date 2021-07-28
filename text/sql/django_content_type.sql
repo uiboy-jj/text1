aef8dfeacc040766c660f620feb5c2729cb04a5e
yjj <2651876603@qq.com>
2021-07-29 03:31:00 +0800

    version1

---
+++ text/sql/django_content_type.sql
@@ -0,0 +1,9 @@
+INSERT INTO "django_content_type" ("id", "app_label", "model") VALUES ('1', 'admin', 'logentry');
+INSERT INTO "django_content_type" ("id", "app_label", "model") VALUES ('3', 'auth', 'group');
+INSERT INTO "django_content_type" ("id", "app_label", "model") VALUES ('2', 'auth', 'permission');
+INSERT INTO "django_content_type" ("id", "app_label", "model") VALUES ('4', 'auth', 'user');
+INSERT INTO "django_content_type" ("id", "app_label", "model") VALUES ('5', 'contenttypes', 'contenttype');
+INSERT INTO "django_content_type" ("id", "app_label", "model") VALUES ('7', 'myapp', 'notice');
+INSERT INTO "django_content_type" ("id", "app_label", "model") VALUES ('8', 'myapp', 'table_five');
+INSERT INTO "django_content_type" ("id", "app_label", "model") VALUES ('9', 'myapp', 'table_six');
+INSERT INTO "django_content_type" ("id", "app_label", "model") VALUES ('6', 'sessions', 'session');
---
+++ text/sql/sqlite_sequence.sql
@@ -0,0 +1,1 @@
+INSERT INTO "sqlite_sequence" ("name", "seq") VALUES ('django_migrations', '20');
